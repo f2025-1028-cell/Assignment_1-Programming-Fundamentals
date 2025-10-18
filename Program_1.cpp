317462a1174efed11251b226b4158bbd65b424ca
f2025-1028-cell <f2025-1028@bnu.edu.pk>
2025-10-17 21:31:08 -0700

    Upload Program_1.cpp

---
+++ Program_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+           cout<< "* *" <<endl;
+           cout<<"** **" <<endl;
+           cout<<"*** ***" <<endl;
+           cout<<"**** ****" <<endl;
+           cout<<"**********" <<endl;
+           cout<<"My name is Mohkum din and I am 18 years old. I am from Ghotki Pakistan" <<endl;
+             return 0;
+}
